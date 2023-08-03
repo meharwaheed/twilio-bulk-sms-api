7ddb16744e7d841268b5763c5a063e2d1efcc9fa
Abdul Waheed <meharwaheed4313@gmail.com>
2023-08-04 04:02:26 +0500

    authentication completed

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,12 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function respond($data = [], string $message = 'Request Successful', bool $success = true, int $code = 200) {
+        return response()->json([
+            'success' => $success,
+            'message' => $message,
+            'data' => $data,
+        ], $code);
+    }
 }
